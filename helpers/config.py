37fb6cafe1d4ac77d0803ff6d29312295ce300eb
BraveRam <99811385+BraveRam@users.noreply.github.com>
2023-09-17 14:43:35 +0300

    Create config.py

---
+++ helpers/config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "6526096598:AAEDG7q_MqUwiEAaYqWVBt91fbBhoOEXR3I"
+MONGO_DB_URL = "mongodb+srv://really651:gSPMW6u9WuStXIwD@cluster0.pxc2foz.mongodb.net/?retryWrites=true&w=majority"
+ADMINS_ID = [5546734859] 
