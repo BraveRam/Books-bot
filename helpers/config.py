a2b694149e62d4f3e52be18868d01d6781454048
BraveRam <99811385+BraveRam@users.noreply.github.com>
2023-09-17 14:48:30 +0300

    Update config.py

--- helpers/config.py
+++ helpers/config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "6526096598:AAEDG7q_MqUwiEAaYqWVBt91fbBhoOEXR3I"
+BOT_TOKEN = "6459660214:AAGzDzoI8UlkVW7bHl62ZC5v1X78BRb6F7g"
 MONGO_DB_URL = "mongodb+srv://really651:gSPMW6u9WuStXIwD@cluster0.pxc2foz.mongodb.net/?retryWrites=true&w=majority"
-ADMINS_ID = [5546734859] 
+ADMINS_ID = [1365625365] 
