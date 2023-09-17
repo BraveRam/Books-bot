47b4b2c1ba163d39227ddf3ab5d5ad298cb54cbb
BraveRam <99811385+BraveRam@users.noreply.github.com>
2023-09-17 14:41:57 +0300

    Create buttons.py

---
+++ helpers/buttons.py
@@ -0,0 +1,18 @@
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+def welcome_btns():
+	keyboard = InlineKeyboardMarkup()
+	keyboard.add(InlineKeyboardButton("My Channel", url="t.me/mt_projectz"), InlineKeyboardButton("Support Group", url="t.me/mt_projectzchat"))
+	keyboard.add(InlineKeyboardButton("Find Books", switch_inline_query_current_chat="JavaScript"), InlineKeyboardButton("Get Help", callback_data="help"))
+	keyboard.add(InlineKeyboardButton("Terms of service", callback_data="tos"))
+	return keyboard
+	
+def tos_btn():
+	keyboard = InlineKeyboardMarkup()
+	keyboard.add(InlineKeyboardButton("✨Continue✨", callback_data="continue"))	
+	return keyboard
+
+def back_btn():
+	keyboard = InlineKeyboardMarkup()
+	keyboard.add(InlineKeyboardButton("🔙Back", callback_data="back"))
+	return keyboard 
