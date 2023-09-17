50a99c255d4aedad34f29de887043af78028cbda
BraveRam <99811385+BraveRam@users.noreply.github.com>
2023-09-17 19:57:21 +0300

    Update utility.py

--- helpers/utility.py
+++ helpers/utility.py
@@ -3,28 +3,31 @@ WELCOME_MSG = """
 
 This bot helps you download so many books for free.
 
-You can send me a book\'s or Search it using Inline mode🎉
+You can send me book\'s or Search it using Inline mode🎉
 """
 
 DISCLAIMER_MSG = """
 <i>⚠️You have to accept the terms of service of this bot before continuing⚠️</i>
 
-<b>The content available in this bot is not the developer\'s' of this bot, so If you want to remove your content from this bot, contact @BetterParrot and your contents will be removed.</b>
+<b>The content available in this book is not the developer\'s' of this bot, so If you want to remove your content from this bot, contact @BetterParrot and your contents will be removed.</b>
 
 <u>By clicking 'Accept terms of service' button, You accept the terms of service of this bot🤖</u>
 """
 
 HELP_MSG = """
-<i>You can use this bot:</i>
+<i>You can use this bot in two ways:</i>
 	
-	By sending book\'s name, the bot will provide a books available for a given term.
+	1. By sending book\'s name, the bot will provide a books available for a given term.
 	    You can then download the book you want🎉
 	
+	2. By using Inline mode, the bot will show a books available for a given term on inline mode and you then choose the book you want to Search || Search method🎉
+	   <code>@{} Typescript</code>
+	   
 <i>✨Thanks for using our bot🤖</i>
 """
 
 SUB_MSG = """
-{}, You have to Join this bot\'s developers before downloading books:)
+{}, You have to Join this bot\'s developer before downloading books:)
 
 <b>🎉Join using this link:- @MT_Projectz and If you are a programmer join @programming_Tipz(Optional)</b>
 """
